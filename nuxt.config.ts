2da58d2b7d691107b66d82b960478d6b637c92be
IqroNegoro <iqronegoro0@gmail.com>
2023-08-05 09:33:14 +0700

    initializing

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss"
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet', href: 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+        }
+      ]
+    }
+  },
+  pinia: {
+    autoImports: [
+      "defineStore",
+      "acceptHMRUpdate"
+    ]
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css'
+  },
+})
